a95ecc9fa17bdd5b72bb55fa033a9c3944b706c0
BairUrusov <urusov.bair@yandex.ru>
2022-01-13 12:13:36 +0300

    feat: add basic permissions and location handler IOS

--- src/components/LocationPicker/index.tsx
+++ src/components/LocationPicker/index.tsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, Alert, Button, Text, View } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import { Colors } from '../../constants';
 import { style as s } from './styles';
 
+const getIOSlocationPermission = async () => {
+  try {
+    const res = await Geolocation.requestAuthorization('always');
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+
+  // console.log(res);
+};
+
 export const LocationPicker = () => {
-  const getLocationHandler = () => {
-    return null;
+  const [pickedLocation, setPickedLocation] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+
+  const getLocationHandler = async () => {
+    const permission = await getIOSlocationPermission();
+
+    if (permission !== 'granted') {
+      return Alert.alert('Permission is not granted');
+    }
+
+    try {
+      setIsFetching(true);
+      const location = await Geolocation.getCurrentPosition(
+        (position) => {
+          console.log('success in getting location');
+          console.log(position);
+
+          setPickedLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (err) => {
+          console.log('fail in getting location');
+          console.log(err);
+        },
+        { timeout: 5000 },
+      );
+      console.log(location);
+
+      console.log(pickedLocation);
+    } catch (err) {
+      console.log(err);
+      Alert.alert('Could not fetch location', 'Pleace try again later or pick a location on the map', [
+        { text: 'Okay' },
+      ]);
+    }
+    setIsFetching(false);
   };
 
   return (
     <View style={s.locationPicker}>
       <View style={s.mapPreview}>
-        <Text>No location chosen yet</Text>
+        {isFetching ? <ActivityIndicator size="large" color={Colors.primary} /> : <Text>No location chosen yet</Text>}
       </View>
 
       <Button title="Get User Location" color={Colors.primary} onPress={getLocationHandler} />
